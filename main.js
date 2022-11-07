378ae96ac1164db2a832c3a1450f888fc916b61f
danieldaga <daniel@arhrestauracion.com>
2022-11-07 18:24:24 +0100

    finnisg

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function descCount(num) {
 descCount(15)
 
 function asCount(num) {
-    for (let i = 0; i < num+1; i++) {
+    for (let i = 0; i < num + 1; i++) {
         console.log(i);
-        
+
     }
 }
 asCount(15)
@@ -22,13 +22,13 @@ let dani = {
     name: 'Dani',
     HP: 120,
     damage: 25,
-    attack : (enemy) => enemy.HP -= dani.damage
+    attack: (enemy) => enemy.HP -= dani.damage
 }
 let pablo = {
     name: 'Pablo',
     HP: 100,
     damage: 19,
-    attack : (enemy) => enemy.HP -= pablo.damage
+    attack: (enemy) => enemy.HP -= pablo.damage
 }
 
 
@@ -37,36 +37,15 @@ pablo.attack(dani)
 console.log(dani, pablo);
 
 function fight(playerOne, playerTwo) {
-    
-    while (playerTwo.HP>0) {
-        let numberOfAttack = 0
+
+    let numberOfAttack = 0
+    while (playerTwo.HP > 0) {
         playerOne.attack(playerTwo)
-        numberOfAttack ++
+        numberOfAttack++
         console.log(playerTwo);
     }
-    console.log(`${playerOne.name} ha derrotado a ${playerTwo.name} en  rondas`)
+    console.log(`${playerOne.name} ha derrotado a ${playerTwo.name} en ${numberOfAttack} rondas`)
 
 }
 fight(dani, pablo)
 
-
-// function fight(dani, pablo) {
-//     let numberOfAttacks = 0 //contador
-//     for (let i = 0; i <= pablo.HP; i++) {
-//         dani.attack = (enemy) => {
-//         enemy.HP -= dani.damage
-        
-//             // function fight(Tyler, Chuck) {
-//             // }
-//         }
-//         dani.attack(pablo)
-//         let numberOfAttacks = (i.length)
-//         numberOfAttacks ++
-//     }
-//     console.log(`${dani.name} ha derrotado a ${pablo.name} en ${numberOfAttacks} rondas`)
-    
-
-//     } 
-
-// fight(dani, pablo)
-// console.log(dani, pablo);
